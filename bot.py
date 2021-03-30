c3488acb65285f71c58b518806a513f94e6eedb7
Higherupanddown <higherupanddown@yandex.ru>
2021-03-30 23:35:55 +0300

    0.1.3

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
         headers={
             "Content-Type": "application/json",
-            "authorization": enter,
+            "authorization": "enter",
             "User-agent": "Mozilla/5.0",
             "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
         },
