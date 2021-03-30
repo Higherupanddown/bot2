42eb33610eff531c2d3777bd8ad151d4d4c87968
Higherupanddown <higherupanddown@yandex.ru>
2021-03-30 23:32:05 +0300

    0.1

---
+++ bot.py
@@ -0,0 +1,25 @@
+
+import requests 
+from requests import get, post
+''' post(
+        "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
+        headers={
+            "Content-Type": "application/json",
+            "authorization": enter,
+            "User-agent": "Mozilla/5.0",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
+        json={"slave_id": enter},
+    )
+'''
+post(
+        "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter",
+        headers={
+            "Content-Type": "application/json",
+            "authorization": "enter",
+            "User-agent": "Mozilla/5.0",
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
+        json={"slave_id": enter},
+    )
+ 
